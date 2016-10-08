e0db45441da88275214b62eac39256f1680fdf98
Suthaporn <57160443@go.buu.ac.th>
2016-10-08 15:43:26 +0700

    Add files via upload

---
+++ oop.rb
@@ -0,0 +1,25 @@
+ข้อ 10
+
+class Circle
+    def initialize(PI, radius)
+        @PI = 3.14159
+        @radius = radius
+    end
+    
+    attr_reader :Circle
+    attr_accessor :radius
+    
+    def getArea
+        puts PI * radius * radius
+    end
+    
+    def getCircumference
+        puts 2 * PI * radius
+    end
+
+circle = Circle.new(10)
+pust "Area is #{circle.getArea}"
+pust "Diameter is #{circle.getradius}"
+pust "Circumference is #{circle.getCircumference}"
+    
+end
